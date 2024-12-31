00aa6648e3fe9262a550da48e17a70417e4a6630
Kaue <130503651+KaueBulhoes@users.noreply.github.com>
2024-12-31 17:02:25 -0300

    feat: add form control

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  //Lembrar de importar o ReactiveFormsModule que não entra automaticamente
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
 export class FormsComponent {
-
   results = [];
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    //Dentro será declarado os campos do formulário
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
+
+  onSubmit() {
+    console.log(this.formGroup.value)
+  }
+
+  onClean() {
+    this.formGroup.reset()
+  }
 
 }
