8226f280bcef97ae4cced460f222c9724d95dbe0
Kaue <130503651+KaueBulhoes@users.noreply.github.com>
2024-12-31 18:25:41 -0300

    feat: filter to open units
    Update forms.component.ts

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
+import { Location } from '../../types/location.interface';
 
 @Component({
   selector: 'app-forms',
@@ -11,7 +12,8 @@ import { GetUnitsService } from '../../services/get-units.service';
   styleUrl: './forms.component.scss'
 })
 export class FormsComponent {
-  results = [];
+  results: Location[] = [];
+  filtredResults: Location [] = [];
   formGroup!: FormGroup;
 
   constructor(
@@ -20,16 +22,25 @@ export class FormsComponent {
   ) {}
 
   ngOnInit(): void {
-    this.unitService.getAllUnits().subscribe(data => console.log(data))
     //Dentro será declarado os campos do formulário
     this.formGroup = this.formBuilder.group({
       hour: '',
-      showClosed: false
+      showClosed: true
+    })
+    //Chamando o service
+    this.unitService.getAllUnits().subscribe(data => {
+      this.results = data.locations;
+      this.filtredResults = data.locations;
     })
   }
 
   onSubmit() {
-    console.log(this.formGroup.value)
+    //Se o valor do formGroupd NÃO for showClosed
+    if(!this.formGroup.value.showClosed) {
+      this.filtredResults = this.results.filter(location => location.opened === true)
+    } else {
+      this.filtredResults = this.results
+    }
   }
 
   onClean() {
