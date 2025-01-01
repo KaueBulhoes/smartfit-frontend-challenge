419300f5382150165d21fd14d7b3225f43ec51e9
Kaue <130503651+KaueBulhoes@users.noreply.github.com>
2025-01-01 19:10:19 -0300

    feat: add legends component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FormsComponent } from "./components/forms/forms.component";
 import { BehaviorSubject } from 'rxjs';
@@ -7,13 +6,14 @@ import { CardsListComponent } from "./components/cards-list/cards-list.component
 import { CommonModule } from '@angular/common';
 import { Location } from './types/location.interface';
 import { GetUnitsService } from './services/get-units.service';
+import { LegendsComponent } from "./components/legends/legends.component";
 
 // Devido ao uso de forma assíncrona no HTML, devemos importar o CommonModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FormsComponent, CardsListComponent, CommonModule],
+  imports: [HeaderComponent, FormsComponent, CardsListComponent, CommonModule, LegendsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
